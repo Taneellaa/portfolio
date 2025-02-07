f715be7282364e6c483f6defe69e4191d662bfd8
mcl0vin34 <8ug8ear8ounce@gmail.com>
2025-02-07 16:24:37 +0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="p-8 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h2>This is my personal portfolio my man</h2>
+      </main>
+
+      <p>xd</p>
+    </div>
+  );
+}
